fadc8867ff29c46f736f29a990742247b8b2dafa
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-08-16 14:59:59 -0300

    feat: requisito 12

--- app/backend/src/middlewares/error.ts
+++ app/backend/src/middlewares/error.ts
@@ -7,6 +7,8 @@ const errorMiddleware: ErrorRequestHandler = (err, req, res, _next) => {
   }
 
   if (err instanceof JsonWebTokenError) {
+    console.log(err);
+
     return res.status(401).json({ message: err.message });
   }
   return res.status(500).json({ message: err.message });
