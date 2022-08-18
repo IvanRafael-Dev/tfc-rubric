e75fe13f2635a2157b5d498814161cf642a6bd41
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-08-18 15:20:45 -0300

    feat: verifica existencia do token

--- app/backend/src/middlewares/auth.ts
+++ app/backend/src/middlewares/auth.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import JWT from '../utils/jwtUtils/JWT';
 
 const validator = new JWT();
 
-const validateToken = (request: Request, response: Response, next: NextFunction) => {
-  const token = request.headers.authorization as string;
+const validateToken = (request: Request, _response: Response, next: NextFunction) => {
+  const token = request.headers.authorization;
+  if (!token) {
+    throw new JsonWebTokenError('Token must be a valid token');
+  }
   const payload = validator.verify(token);
   request.body.user = payload;
   next();
